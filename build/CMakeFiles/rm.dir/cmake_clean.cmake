20c821557cd7acc0bacbea90a8521ed3050e3615
lucaparmigiani <luca.parmigiani@studenti.univr.it>
2020-07-13 01:49:16 +0200

    removed other

--- build/CMakeFiles/rm.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/rm"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/rm.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
